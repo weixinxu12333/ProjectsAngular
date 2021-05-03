8298205fc036f1e70366aaf7be4082a4dc6238af
Weixin Xu <wxusmx2013v@gmail.com>
2021-05-04 00:16:38 +0200

    App service

---
+++ src/app/new-account/new-account.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { AccountsService } from '../accounts.service';
+import { LoggingService } from '../logging.service';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css'],
+  // providers: [LoggingService]
+})
+export class NewAccountComponent {
+
+  constructor(private loggingService: LoggingService, private accountService: AccountsService){
+    this.accountService.statusUpdated.subscribe(
+      (status: string) => alert('New status:' + status)
+    );
+   }
+
+  onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountService.addAccount(accountName, accountStatus);
+    // this.loggingService.logStatusChange(accountStatus);
+
+    // const service = new LoggingService();
+    // service.logStatusChange(accountStatus);
+  }
+}
